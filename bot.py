b8069ea08d6a5da678a5b525161a180bcbf019d1
ebrahimbaram3745-gif <ebrahimbaram3745@gmail.com>
2026-05-31 22:10:28 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,17 @@
+import json
 import os
+
+def load_data(filename):
+    if not os.path.exists(filename):
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump({}, f)
+
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_data(filename, data):
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 from flask import Flask
 from threading import Thread
