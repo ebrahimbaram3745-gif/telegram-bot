0e84456231dac3e0d323539169523544a7ee2cae
ebrahimbaram3745-gif <ebrahimbaram3745@gmail.com>
2026-06-21 02:10:16 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,7 +65,9 @@ SECOND_ADMIN_ID = 8489061532
 CARD_NUMBER = "6219861449318822"
 
 # ─── تنظیمات پنل Nahan ───────────────────────────────────────────
-NAHAN_URL = "https://proud-dew-0f73.itx-mm2di1000.workers.dev/sync/dash"
+NAHAN_URL = "https://proud-dew-0f73.itx-mm2di1000.workers.dev"
+NAHAN_API_ROUTE = "sync"
+NAHAN_API_ROUTE = "dash"
 NAHAN_PASSWORD = "admin"
 
 def nahan_build_sub_url(username: str) -> str:
